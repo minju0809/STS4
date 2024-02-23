d9e8837dd07f464fa7e5d753a8fb1d1d26e5fe67
minju0809 <loverminju88@naver.com>
2024-02-23 11:18:00 +0900

    240223 - 애플리케이션 배포 시험

---
+++ P02230/src/main/java/com/rubypaper/P02230Application.java
@@ -0,0 +1,13 @@
+package com.rubypaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P02230Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P02230Application.class, args);
+	}
+
+}
---
+++ P02230/src/test/java/com/rubypaper/P02230ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubypaper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P02230ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
